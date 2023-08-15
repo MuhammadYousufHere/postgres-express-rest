6efb91da3e10d7f96f9305e1d03a7ecff95b0484
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-08-15 18:36:54 +0500

    add db con uri

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,8 @@
+import 'dotenv/config'
+
 export default {
-  port: 8080,
+  port: process.env.PORT || 1234,
   saltRounds: process.env.BCRYPT_SALT_ROUNDS ? parseInt(process.env.BCRYPT_SALT_ROUNDS, 10) : 10,
+  dbUri: process.env.DB_CONNECTION,
+  LOG_LEVEL: process.env.LOG_LEVEL || 'info',
 }
