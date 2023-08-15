58c5c02158cd097feffa3b8de8f5fd13fb726dcd
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-08-15 20:55:07 +0500

    add sec key

--- config/default.ts
+++ config/default.ts
@@ -4,5 +4,6 @@ export default {
   port: process.env.PORT || 1234,
   saltRounds: process.env.BCRYPT_SALT_ROUNDS ? parseInt(process.env.BCRYPT_SALT_ROUNDS, 10) : 10,
   dbUri: process.env.DB_CONNECTION,
-  LOG_LEVEL: process.env.LOG_LEVEL || 'info',
+  logLevel: process.env.LOG_LEVEL || 'info',
+  accessTokenSecret: process.env.ACCESS_TOKEN_PRIVATE_KEY,
 }
