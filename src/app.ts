c2c8be29cdb239746aa98d783569f3f336ad87bd
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-08-15 20:56:24 +0500

    wip: app

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ import config from 'config'
 import responseTime from 'response-time'
 import dayjs from 'dayjs'
 import cors from 'cors'
-
+import helmet from 'helmet'
+import cookiesParser from 'cookie-parser'
 //utils
 import logger from './utils/logger'
 import { getDB, waitDBConnect } from './utils/db-client'
+import { errorHandler } from './middleware/errorHandlers'
+import path from 'path'
 
 // const pool = new Pool({
 //   port: 5432,
@@ -21,9 +24,22 @@ const app = express()
 const port = config.get<number>('port')
 const dbUri = config.get<string>('dbUri')
 
-app.use(express.json({}))
+// built-in middleware for json
+app.use(express.json())
 app.use(cors())
+app.use(helmet())
+//middleware for cookies
+app.use(cookiesParser())
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')))
 
+app.use(
+  responseTime((req: Request, _res: Response, time: number) => {
+    if (req.route?.path) {
+      console.log(time * 1000)
+    }
+  })
+)
 function shutDown() {}
 function start() {
   process.on('unhandledRejection', (error: any, promise) => {
@@ -43,6 +59,8 @@ function start() {
 
 //create table users
 GET('/users/create', () => getDB(dbUri).users.create())
+GET('/users/init', () => getDB(dbUri).users.init())
+GET('/users/drop', () => getDB(dbUri).users.drop())
 
 app.use('/status', (_req: Request, res: Response) => {
   res.json({
@@ -69,14 +87,24 @@ function GET(url: string, handler: (req: any) => any) {
   })
 }
 
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' })
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
+})
+
+app.use(errorHandler)
 app.listen(port, async () => {
   logger.info(` ⚡️ App is up and running on port localhost:${port}`)
   return waitDBConnect(getDB(dbUri))
-    .then(db => {
-      console.log(db.client.user)
-    })
+    .then(() => {})
     .catch(err => {
-      logger.info(`Failed to connect DB : ${err.stack}`)
+      logger.error(`Failed to connect DB : ${err.stack}`)
       process.exit(1)
     })
 })
