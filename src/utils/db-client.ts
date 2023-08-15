302ebc73cd19e153a8e1c256ec6512fc5c9d172c
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-08-15 18:35:03 +0500

    complete bd conn utils

--- src/utils/db-client.ts
+++ src/utils/db-client.ts
@@ -1,7 +1,13 @@
-import pgPromise, { IEventContext, IDatabase, IInitOptions } from 'pg-promise'
+import pgPromise, { IEventContext, IDatabase, IInitOptions, IMain } from 'pg-promise'
 import asyncRetry from 'async-retry'
+import log from './logger'
+import { IExtensions } from '../../@types'
+import { Diagnostic } from './diagnostic'
+import { Users } from '../../db/user'
 
-const pgConfig: IInitOptions = {
+type ExtendedProtocol = IDatabase<IExtensions> & IExtensions
+
+const pgConfig: IInitOptions<IExtensions> = {
   capSQL: true,
   noWarnings: true,
   query: (e: IEventContext) => {
@@ -9,23 +15,29 @@ const pgConfig: IInitOptions = {
       console.log(e.query)
     }
   },
+  extend(obj: ExtendedProtocol, _dc: any) {
+    obj.users = new Users(obj, pgP)
+  },
 }
 
-const pgP = pgPromise({
-  ...pgConfig,
-})
+// Initializing the library
+const pgP: IMain = pgPromise(pgConfig)
 
 pgP.pg.defaults.max = 50 // set pool size to 50
 
-function getDB(cn: string) {
+function getDB(cn: string): ExtendedProtocol {
   return pgP(cn)
 }
+
+// Initializing optional diagnostics
+Diagnostic.init(pgConfig)
 
 function waitDBConnect(db: IDatabase<any>, retries = 5) {
   return asyncRetry(
     async () => {
       const connection = await db.connect()
       await connection.done()
+      log.info('DB Connected!')
       return connection
     },
     { retries }
