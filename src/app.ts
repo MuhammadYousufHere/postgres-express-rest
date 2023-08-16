ce55dbdfe52ae6f575d504dc3819c5b85f8655ee
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-08-16 20:55:50 +0500

    wip: app

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,59 @@
 import express, { Request, Response } from 'express'
-import stoppable from 'stoppable'
+import stoppable, { StoppableServer } from 'stoppable'
 import config from 'config'
 import responseTime from 'response-time'
 import dayjs from 'dayjs'
 import cors from 'cors'
 import helmet from 'helmet'
+import compression from 'compression'
 import cookiesParser from 'cookie-parser'
-//utils
-import logger from './utils/logger'
-import { getDB, waitDBConnect } from './utils/db-client'
-import { errorHandler } from './middleware/errorHandlers'
 import path from 'path'
 
-// const pool = new Pool({
-//   port: 5432,
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'password123@',
-//   database: 'my-pg-db',
-// })
+//utils
+import logger from './utils/logger'
+import { getDB, waitDBConnect } from '../config/db-client'
+import { errorHandler } from './middleware/errorHandlers'
+import { corsOptions } from '../config/corsOptions'
+import { shouldCompress } from '../config/compressConfig'
+import { validateEnv } from './utils/validation'
+
+const killSignals = {
+  SIGHUP: 1,
+  SIGINT: 2,
+  SIGUSR2: 12,
+  SIGTERM: 15,
+}
+
+validateEnv()
+
 const app = express()
 
 const port = config.get<number>('port')
+const host = config.get<string>('host')
 const dbUri = config.get<string>('dbUri')
 
 // built-in middleware for json
 app.use(express.json())
-app.use(cors())
-app.use(helmet())
+//cors middlware
+app.use(cors(corsOptions))
+
+//
+app.use(compression({ filter: shouldCompress, threshold: 0 }))
+/**
+ * Adds security middleware to app
+ */
+app.use(helmet({ noSniff: true, xssFilter: true, ieNoOpen: true, hsts: true, frameguard: true, hidePoweredBy: true }))
+
 //middleware for cookies
 app.use(cookiesParser())
-//serve static files
-app.use('/', express.static(path.join(__dirname, '/public')))
 
+app.use(errorHandler)
+
+app.set('json spaces', 2)
+//serve static files
+app.use('/', express.static(path.join(__dirname, '../public')))
+
+// use for computing processing time on response
 app.use(
   responseTime((req: Request, _res: Response, time: number) => {
     if (req.route?.path) {
@@ -40,8 +61,30 @@ app.use(
     }
   })
 )
-function shutDown() {}
+
+// close server
+function shutdown(nodeApp: StoppableServer, signal: string, value: number) {
+  logger.info(`Trying shutdown, got signal ${signal}`)
+  nodeApp.stop(() => {
+    logger.info('Server stopped gracefuly.')
+    getDB(dbUri).$pool.end()
+    logger.info('DB connections stopped.')
+    process.exit(128 + value)
+  })
+}
+
+// start app
+// has arg of context
 function start() {
+  const nodeApp = stoppable(
+    app.listen({ port, host }, async () => {
+      logger.info(` ⚡️ App is up and running at http://${host}:${port}`)
+    })
+  )
+
+  nodeApp.timeout = 0
+  nodeApp.keepAliveTimeout = 6000 // 60 secs
+
   process.on('unhandledRejection', (error: any, promise) => {
     logger.error(`unhandledRejection at: ${promise} `, {
       stack: error.stack,
@@ -55,6 +98,11 @@ function start() {
       error: JSON.stringify(error),
     })
   })
+  process.once('SIGUSR2', () => shutdown(nodeApp, 'SIGUSR2', killSignals.SIGUSR2))
+  process.once('SIGHUP', () => shutdown(nodeApp, 'SIGHUP', killSignals.SIGHUP))
+  process.once('SIGINT', () => shutdown(nodeApp, 'SIGINT', killSignals.SIGINT))
+  process.once('SIGTERM', () => shutdown(nodeApp, 'SIGTERM', killSignals.SIGTERM))
+  return nodeApp
 }
 
 //create table users
@@ -68,6 +116,25 @@ app.use('/status', (_req: Request, res: Response) => {
     timestamp: dayjs().format('YYYY-MM-DD HH:mm:ssZ'),
   })
 })
+
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' })
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
+})
+
+waitDBConnect(getDB(dbUri))
+  .then(start)
+  .catch(err => {
+    logger.error(`Error while connecting to the database : ${err.stack}`)
+    logger.error(`Failed to start services : ${err.stack}`)
+    process.exit(1)
+  })
 
 // Generic GET handler;
 function GET(url: string, handler: (req: any) => any) {
@@ -86,25 +153,3 @@ function GET(url: string, handler: (req: any) => any) {
     }
   })
 }
-
-app.all('*', (req, res) => {
-  res.status(404)
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
-  } else if (req.accepts('json')) {
-    res.json({ error: '404 Not Found' })
-  } else {
-    res.type('txt').send('404 Not Found')
-  }
-})
-
-app.use(errorHandler)
-app.listen(port, async () => {
-  logger.info(` ⚡️ App is up and running on port localhost:${port}`)
-  return waitDBConnect(getDB(dbUri))
-    .then(() => {})
-    .catch(err => {
-      logger.error(`Failed to connect DB : ${err.stack}`)
-      process.exit(1)
-    })
-})
